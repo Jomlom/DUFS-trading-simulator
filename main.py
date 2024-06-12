c8e97a90547a5c89b82d0f97661c7ced440dfb2e
BicknellF <57177864+BicknellF@users.noreply.github.com>
2024-06-12 22:33:07 +0100

    First working main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from datamodel import *
 from dataimport import *
 from Runscript import *
 from datetime import datetime
+import matplotlib.pyplot as plt
 
 
 #### CURRENT ISSUES
@@ -10,8 +11,9 @@ Orders ordering too much, specifically when trying to buy
 """
 class Portfolio:
     def __init__(self) -> None:
-        self.cash = 100000
+        self.cash = 0
         self.quantity = 0
+        self.value = 0
 
 portfolio = Portfolio()
 
@@ -25,8 +27,10 @@ ticks = len(df)
 
 from examplealgo import Trader
 
+vis = []
 start = datetime.now()
-for tick in range(0, ticks):
+for tick in range(0, 5000):
+    print(tick)
     market_listings = extract_orders(df, tick)
     sell_orders = market_listings[1]
     buy_orders = market_listings[0]
@@ -44,50 +48,49 @@ for tick in range(0, ticks):
 
         # order matching
         for order in orders: 
-            print("!!")
-            print(order.quantity)
-            print(order.price)
-            print("!!")
             if order.quantity < 0: # sell orders
                 #match order with a buy order
                 quantity = order.quantity
                 for i in range(len(buy_orders)):
                     if buy_quantities[i] != 0:
                         if buy_prices[i] >= order.price:
-                            print(f"Quantity at {buy_prices[i]} = {buy_quantities[i]}")
                             fulfilled_amount = min(int(pos_limit - abs(portfolio.quantity)), -quantity, buy_quantities[i]) #quantity before order limit, order quantity remaining, quantity avaliable, 
-                            print(fulfilled_amount)
                             portfolio.quantity -= fulfilled_amount
                             buy_quantities[i] -= fulfilled_amount
                             portfolio.cash += fulfilled_amount * order.price
+                            portfolio.value -= fulfilled_amount * order.price
                             quantity += fulfilled_amount
                     if quantity == 0 or buy_prices[i] < order.price:
-                        print("OUT")
+                        #print("OUT")
                         break
-                    print(f"quantity left = {quantity}")
+                    #print(f"quantity left = {quantity}")
 
             elif order.quantity > 0: # buy orders
                 quantity = order.quantity
                 for i in range(0,len(buy_orders)):
                     if sell_quantities[i] != 0:
-                        print(f"sell price = {sell_prices[i]}, our order = {order.price}")
+                        #print(f"sell price = {sell_prices[i]}, our order = {order.price}")
                         if sell_prices[i] <= order.price:
-                            print(f"Quantity at {sell_prices[i]} = {sell_quantities[i]}")
-                            fulfilled_amount = min(int(pos_limit - abs(portfolio.quantity)), order.quantity, sell_quantities[i])
-                            print(f"ffd amt:{fulfilled_amount}")
+                            #print(f"Quantity at {sell_prices[i]} = {sell_quantities[i]}")
+                            fulfilled_amount = min(int(pos_limit - abs(portfolio.quantity)), quantity, sell_quantities[i])
+                            #print(f"ffd amt:{fulfilled_amount} out of qty: {quantity}")
                             portfolio.quantity += fulfilled_amount
                             sell_quantities[i] += fulfilled_amount
                             portfolio.cash -= fulfilled_amount * order.price
+                            portfolio.value += fulfilled_amount * order.price
                             quantity -= fulfilled_amount
                     if quantity == 0 or sell_prices[i] > order.price:
-                        print("OUT")
+                        #print("OUT")
                         break
-                    print(f"quantity left = {quantity}")
-    break
+                    #print(f"quantity left = {quantity}")
+    vis.append(portfolio.quantity)
 
 end = datetime.now()
 
-print(end-start)
+# roughly 0:00:00.001054 per tick
+# roughly 10 seconds per day
+print((end-start))
 
 print(portfolio.quantity)
-    
+plt.plot(vis)
+plt.show()
