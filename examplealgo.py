a272dde75eb6e0aae4ee3fb9acf53d1121eede24
Tim-Wolstenholme <timwolstenholme06@gmail.com>
2025-11-15 15:02:32 +0000

    Update examplealgo.py

--- examplealgo.py
+++ examplealgo.py
@@ -1,18 +1,18 @@
 from datamodel import *
 
+
 class Trader:
-    last_price = 0
     def run(self, state):
         orders = []
         for product in state.products:
             listings = Listing(state.orderbook[product], product)
-
             highest_bid = list(listings.buy_orders.keys())[0]
             bid_quantities = list(listings.buy_orders.values())
 
             lowest_ask = list(listings.sell_orders.keys())[0]
             ask_quantities = list(listings.sell_orders.values())
-
-            orders.append(Order(product, lowest_ask, 5)) #Buying
-            orders.append(Order(product, highest_bid, -5)) #Selling
+            if best_bid > 10000:
+                orders.append(Order(product, 100, -5))
+            if best_ask < 10000:
+                orders.append(Order(product, best_ask, 5))
         return orders
